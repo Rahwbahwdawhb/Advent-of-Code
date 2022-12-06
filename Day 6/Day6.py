7ba3242c08a259eb31c2078475a1b7482c15fb5b
Rahwbahwdawhb <119530761+Rahwbahwdawhb@users.noreply.github.com>
2022-12-06 06:29:26 +0100

    Day 6 finished

---
+++ Day 6/Day6.py
@@ -0,0 +1,33 @@
+from preload import input
+# print(input)
+
+# input='mjqjpqmgbljsphdztnvjfqwrcgsmlb'
+# input='bvwbjplbgvbhsrlpgdmjqwftvncz'
+#1st problem
+inpList=input.split('\n')
+inpStr=inpList[0]
+# print(len(inpList),inpList[0])
+# print(len(inpStr))
+n=4
+subStrs=[inpStr[i:i+n] for i in range(0,len(inpStr))]
+iter=0
+for subStr in subStrs:
+    # print(subStr)
+    uChars=''.join(set(subStr))
+    if len(uChars)==4:
+        break
+    iter+=1
+print('1st: ',iter+4)
+
+#2nd problem
+n=14
+subStrs=[inpStr[i:i+n] for i in range(0,len(inpStr))]
+iter=0
+for subStr in subStrs:
+    # print(subStr)
+    uChars=''.join(set(subStr))
+    if len(uChars)==14:
+        break
+    iter+=1
+print('2nd: ',iter+14)
+
